6fec3a90210b94f2a8c5a6ecd43d982a37899264
LebogangMputle <lebozikalala@gmail.com>
2024-03-20 18:37:46 +0200

    completed challenge 1-4 left with 5

--- index.js
+++ index.js
@@ -5,15 +5,24 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+
+// Checking the user's preference and printing the appropriate message
+if (userPreference === 'Regular') {                 
+  console.log("User prefers Regular Monster Energy drink"); //Print message indicating user preference for Regular Monster Energy drink.
+} else if (userPreference === 'Sugar-free') {       
+  console.log("User prefers Sugar-free Monster Energy drink");  //Print message indicating user preference for Sugar-free Monster Energy drink.
+} else {                                           
+  console.log("User preference not set, defaulting to Regular Monster Energy drink"); //Print message indicating that user preference is not set and defaulting to Regular Monster Energy drink.
+}
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
-let cansLeft = 3; // Any number of cans
-let messagePosted = cansLeft < 5 ? "Time to restock!" : "We're stocked!";
-console.log(messagePosted);
+let cansLeft = 3; // Any number of cans.
+// Using a ternary operator to determine the message to be printed based on the number of cans left.
+let messagePosted = cansLeft < 5 ? "Time to restock!" : "We're stocked!";   // checks if the value of cansLeft is less than 5.
+console.log(messagePosted);  // Printing the appropriate message based on the number of cans left.
 
 
 // #### Challenge 3: Workout Intensity
@@ -21,8 +30,8 @@ console.log(messagePosted);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-let message = heartRate < 100 ? "Boost needed!" : "Energy levels are high!";
-console.log(message);
+let message = heartRate < 100 ? "Boost needed!" : "Energy levels are high!";    // checks if the value of the heartRate is less than 100 bpm.
+console.log(message);   // Printing the appropriate message based on the heartrate
 
 
 // #### Challenge 4: Temperature Suitability
@@ -30,7 +39,8 @@ console.log(message);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+let outPutMessage = currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cooler!";
+console.log(outPutMessage);
 
 
 // #### Challenge 5: Late Night Coding Session
